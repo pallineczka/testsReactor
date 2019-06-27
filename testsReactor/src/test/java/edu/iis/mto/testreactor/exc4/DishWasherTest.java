9d912a079f5f83aa1c9080bb0abce5a6d8db6c5f
pallineczka <paulina.stelmasiak97@gmail.com>
2019-06-27 15:59:07 +0200

    add TestDishWasherWithECOWashingProgramAndTabletsUsed

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
@@ -1,15 +1,42 @@
 package edu.iis.mto.testreactor.exc4;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
+import org.junit.Before;
 import org.junit.Test;
-
+import org.mockito.Mockito;
 
 public class DishWasherTest {
 
+    WaterPump waterPump;
+    Engine engine;
+    DirtFilter dirtFilter;
+    Door door;
+    DishWasher dishWasher;
+
+    @Before
+    public void initialize(){
+
+        waterPump = mock(WaterPump.class);
+        engine = mock(Engine.class);
+        dirtFilter = mock(DirtFilter.class);
+        door = mock(Door.class);
+
+        dishWasher = new DishWasher(waterPump, engine, dirtFilter, door);
+    }
+
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void TestDishWasherWithECOWashingProgramAndTabletsUsed(){
+
+        ProgramConfiguration program = ProgramConfiguration.builder()
+                                                           .withProgram(WashingProgram.ECO)
+                                                           .withTabletsUsed(true)
+                                                           .build();
+        Mockito.when(door.closed()).thenReturn(false);
+        Mockito.when(dirtFilter.capacity()).thenReturn(60.0d);
+
+
+        dishWasher.start(program);
     }
 
 }
