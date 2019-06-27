cf7cc6b4882944e63b1dff0ad110f22de6f3ec56
pallineczka <paulina.stelmasiak97@gmail.com>
2019-06-27 16:23:38 +0200

    add TestProgramShouldHaveTheSameWashingProgram

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
@@ -1,9 +1,11 @@
 package edu.iis.mto.testreactor.exc4;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.mockito.Mockito.mock;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
 public class DishWasherTest {
@@ -35,8 +37,33 @@ public class DishWasherTest {
         Mockito.when(door.closed()).thenReturn(false);
         Mockito.when(dirtFilter.capacity()).thenReturn(60.0d);
 
+        dishWasher.start(program);
+    }
+
+    @Test (expected = PumpException.class)
+    public void TestDishWasherWithECOWashingProgramAndTabletsNotUsed(){
+
+        ProgramConfiguration program = ProgramConfiguration.builder()
+                                                           .withProgram(WashingProgram.INTENSIVE)
+                                                           .withTabletsUsed(false)
+                                                           .build();
+        Mockito.when(door.closed()).thenReturn(false);
+        Mockito.when(dirtFilter.capacity()).thenReturn(5.0d);
+        //Mockito.when(waterPump.pour(WashingProgram.INTENSIVE)).thenReturn(ArgumentMatchers.any());
 
         dishWasher.start(program);
     }
 
+    @Test
+    public void TestProgramShouldHaveTheSameWashingProgram(){
+
+        ProgramConfiguration program = ProgramConfiguration.builder()
+                                                           .withProgram(WashingProgram.INTENSIVE)
+                                                           .withTabletsUsed(false)
+                                                           .build();
+        Mockito.when(door.closed()).thenReturn(false);
+        Mockito.when(dirtFilter.capacity()).thenReturn(5.0d);
+
+        assertEquals(program.getProgram(), WashingProgram.INTENSIVE);
+    }
 }
